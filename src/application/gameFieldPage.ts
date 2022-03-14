c3586a8cfb4f2a668f9b2b9c3109c94778d11497
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-03-14 18:32:14 +0300

    feat: add indicators on gamefield

--- src/application/gameFieldPage.ts
+++ src/application/gameFieldPage.ts
@@ -1,18 +1,21 @@
 import Control from '../common/control';
+import { ArtistQuestionView } from './artistQuestionView';
+import { IArtistQuestionData } from './IArtistQuestionData';
 
 interface IQuizOptions {
   gameName: string;
   categoryIndex: number;
 }
 
-interface IQuizResults {
-
-}
+type IQuizResults = Array<boolean>;
 
 export class GameFieldPage extends Control {
   onBack: () => void;
   onHome: () => void;
   onFinish: (results: IQuizResults) => void;
+  progressIndicator: Control<HTMLElement>;
+  results: IQuizResults;
+  answersIndicator: Control<HTMLElement>;
 
   constructor(parentNode: HTMLElement, gameOptions: IQuizOptions) {
     super (parentNode);
@@ -28,9 +31,34 @@ export class GameFieldPage extends Control {
       this.onHome();
     }
 
-    const finishButton = new Control(this.node, 'button', '', 'finish');
-    finishButton.node.onclick = () => {
-      this.onFinish({});
+    this.progressIndicator = new Control(this.node, 'div', '', '');
+    this.answersIndicator = new Control(this.node, 'div', '', '');
+
+    const questions: Array<IArtistQuestionData> = [{answers: [1, 2, 3, 4], correctAnswerIndex: 1}, {answers: [1, 2, 3, 4], correctAnswerIndex: 3}, {answers: [1, 2, 3, 4], correctAnswerIndex: 4}];
+    this.results = []; 
+    
+    this.questionCycle(questions, 0, () => {
+      this.onFinish(this.results);
+    });
+
+    // const finishButton = new Control(this.node, 'button', '', 'finish');
+    // finishButton.node.onclick = () => {
+    //   this.onFinish({});
+    // }
+  }
+
+  questionCycle(questions: Array<IArtistQuestionData>, index: number, onFinish: () => void) {
+    if (index >= questions.length) {
+      onFinish();
+      return;
+    }
+    this.progressIndicator.node.textContent = `${index + 1} / ${questions.length}`;
+    this.answersIndicator.node.textContent = this.results.map(it => it ? '+' : '-').join(' ');
+    const question = new ArtistQuestionView(this.node, questions[0]);
+    question.onAnswer = (answerIndex) => {
+      question.destroy();
+      this.results.push(answerIndex === questions[index].correctAnswerIndex);
+      this.questionCycle(questions, index + 1, onFinish);
     }
   }
 }
